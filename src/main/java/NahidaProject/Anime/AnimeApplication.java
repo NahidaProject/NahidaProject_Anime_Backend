22ac6791022c515d3791807a19fd8df9176960a4
baizhi958216 <1475289190@qq.com>
2022-11-09 22:54:16 +0800

    Anime: Init
    Signed-off-by: baizhi958216 <1475289190@qq.com>

---
+++ src/main/java/NahidaProject/Anime/AnimeApplication.java
@@ -0,0 +1,13 @@
+package NahidaProject.Anime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnimeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/NahidaProject/Anime/AnimeApplicationTests.java
@@ -0,0 +1,13 @@
+package NahidaProject.Anime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
