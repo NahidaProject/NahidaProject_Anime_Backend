3cb2bb7812f30abe42b9da6c7b448f3abb1620a2
baizhi958216 <1475289190@qq.com>
2022-11-14 00:24:47 +0800

    Anime: User Register
    Signed-off-by: baizhi958216 <1475289190@qq.com>

--- src/main/java/NahidaProject/Anime/controller/UserRegister.java
+++ src/main/java/NahidaProject/Anime/controller/UserRegister.java
@@ -1,8 +1,27 @@
 package NahidaProject.Anime.controller;
 
+import NahidaProject.Anime.mapper.MainMapper;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @Controller
+@RequestMapping("/api")
 public class UserRegister {
-    
+
+    @Resource
+    MainMapper mapper;
+
+    @RequestMapping(value = "/register",method = RequestMethod.POST,params = {"username","password","role"})
+    public void register(HttpServletRequest request, HttpServletResponse response){
+        mapper.addUser(
+                request.getParameter("username"),
+                request.getParameter("password"),
+                request.getParameter("role"));
+        response.setStatus(200);
+    }
 }
--- src/main/java/NahidaProject/Anime/mapper/MainMapper.java
+++ src/main/java/NahidaProject/Anime/mapper/MainMapper.java
@@ -1,6 +1,7 @@
 package NahidaProject.Anime.mapper;
 
 import NahidaProject.Anime.entity.UserData;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
@@ -8,4 +9,10 @@ import org.apache.ibatis.annotations.Select;
 public interface MainMapper {
     @Select("select * from users where username = #{username}")
     UserData findUserByName(String username);
+
+    @Select("select * from users")
+    UserData findAllUsers();
+
+    @Insert("insert into users (username,password,role) values(#{username},#{password},#{role})")
+    int addUser(String username,String password,String role);
 }
