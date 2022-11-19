f01eab88e7c34f548880385ce025a0268861c777
baizhi958216 <1475289190@qq.com>
2022-11-20 01:19:02 +0800

    Anime: Upload poster/video with a_id
    Signed-off-by: baizhi958216 <1475289190@qq.com>

--- src/main/java/NahidaProject/Anime/controller/FileUploadController.java
+++ src/main/java/NahidaProject/Anime/controller/FileUploadController.java
@@ -1,22 +1,36 @@
 package NahidaProject.Anime.controller;
 
+import NahidaProject.Anime.service.AnimeService;
 import NahidaProject.Anime.service.FileUploadService;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+
+import javax.annotation.Resource;
 
 @RestController
 @RequestMapping("/api")
 @ResponseBody
 @CrossOrigin
 public class FileUploadController {
-    @RequestMapping(value = "/animePosterUpload",method = RequestMethod.POST)
-    public Object animePosterUpload(@RequestParam("poster")MultipartFile poster){
-        return new FileUploadService().saveThisPoster(poster);
+
+    @Resource
+    AnimeService animeService;
+    @RequestMapping(value = "/animePosterUpload/{a_id}",method = RequestMethod.POST)
+    public Object animePosterUpload(@RequestParam("poster")MultipartFile poster, @PathVariable String a_id){
+        return new FileUploadService().saveThisPoster(poster,a_id);
     }
 
-    @RequestMapping(value = "/animeVideosUpload",method = RequestMethod.POST)
-    public Object animeVideosUpload(@RequestParam("videos")MultipartFile videos){
-        return new FileUploadService().saveThisVideo(videos);
+    @RequestMapping(value = "/animeVideosUpload/{a_id}",method = RequestMethod.POST)
+    public Object animeVideosUpload(@RequestParam("videos")MultipartFile videos, @PathVariable String a_id){
+        int a_set_tmp = animeService.findAnimeById(a_id).getA_set();
+        String a_set = "";
+        if (a_set_tmp >= 10) {
+            a_set = "0" + a_set_tmp;
+        } else {
+            a_set = "00" + a_set_tmp;
+        }
+        String videoName = "F:/anime/videos/" + a_id + "/" + a_id + "_" + a_set + ".mp4";
+        return new FileUploadService().saveThisVideo(videos,videoName);
     }
 
 
--- src/main/java/NahidaProject/Anime/service/FileUploadService.java
+++ src/main/java/NahidaProject/Anime/service/FileUploadService.java
@@ -1,5 +1,6 @@
 package NahidaProject.Anime.service;
 
+import NahidaProject.Anime.entity.AnimeData;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -9,26 +10,23 @@ import java.io.IOException;
 @Service
 public class FileUploadService {
 
-    public Object saveThisPoster(MultipartFile poster){
-        String posterName = poster.getOriginalFilename();
-        assert posterName != null;
-        File pFile = new File("F:/anime/main_image/"+posterName);
+    public Object saveThisPoster(MultipartFile poster, String a_id) {
+        String posterName = a_id + ".png";
+        File pFile = new File("F:/anime/main_image/" + posterName);
         try {
             poster.transferTo(pFile);
-        }catch (IOException e){
+        } catch (IOException e) {
             e.printStackTrace();
             return "上传失败";
         }
         return "nia~";
     }
 
-    public Object saveThisVideo(MultipartFile videos) {
-        String posterName = videos.getOriginalFilename();
-        assert posterName != null;
-        File pFile = new File("F:/anime/videos/"+posterName.substring(0,6)+"/"+posterName);
+    public Object saveThisVideo(MultipartFile videos, String videoName) {
+        File pFile = new File(videoName);
         try {
             videos.transferTo(pFile);
-        }catch (IOException e){
+        } catch (IOException e) {
             e.printStackTrace();
             return "上传失败";
         }
